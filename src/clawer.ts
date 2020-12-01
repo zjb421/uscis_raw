4238c3589980284e89e28897ced817bf09c3aef3
James Zhao <james.zhao@onelogin.com>
2020-12-01 10:38:02 -0800

    update

--- src/clawer.ts
+++ src/clawer.ts
@@ -157,7 +157,7 @@ const claw = async (
 };
 
 (async () => {
-  for (const d of lodash.range(230, 244)) {
+  for (const d of lodash.range(200, 245)) {
     await Promise.all(
       Constants.CENTER_NAMES.map((name) => claw(name, 20, d, 5))
     );
